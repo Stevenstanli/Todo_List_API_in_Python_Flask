bec0ef5e40680e264bc99b12aaa85f6ed1c9916c
Steven <stanli-peter19@hotmail.com>
2021-02-23 22:40:26 +0000

    simple apli example

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Favorites
+from models import db, User, Todos
 #from models import Person
 
 app = Flask(__name__)
@@ -40,11 +40,11 @@ def handle_hello():
     return jsonify(response_body), 200
 
 
-@app.route('/get_favorites', methods=['GET'])
+@app.route('/get_todos', methods=['GET'])
 def get_fav():
 
     # get all the favorites
-    query = Favorites.query.all()
+    query = Todos.query.all()
 
     # map the results and your list of people  inside of the all_people variable
     results = list(map(lambda x: x.serialize(), query))
@@ -52,28 +52,28 @@ def get_fav():
     return jsonify(results), 200
 
 
-@app.route('/add_favorite', methods=['POST'])
+@app.route('/add_todos', methods=['POST'])
 def add_fav():
 
     # recibir info del request
     request_body = request.get_json()
     print(request_body)
 
-    fav = Favorites(name=request_body["name"])
+    fav = Todos(name=request_body["name"])
     db.session.add(fav)
     db.session.commit()
 
     return jsonify("All good"), 200
 
 
-@app.route('/update_favorite/<int:fid>', methods=['PUT'])
+@app.route('/update_Todos/<int:fid>', methods=['PUT'])
 def update_fav(fid):
 
     # recibir info del request
     
-    fav = Favorites.query.get(fid)
+    fav = Todos.query.get(fid)
     if fav is None:
-        raise APIException('Favorite not found', status_code=404)
+        raise APIException('Todos not found', status_code=404)
 
     request_body = request.get_json()
 
@@ -85,14 +85,14 @@ def update_fav(fid):
     return jsonify("All good"), 200
 
 
-@app.route('/del_favorite/<int:fid>', methods=['DELETE'])
+@app.route('/del_Todos/<int:fid>', methods=['DELETE'])
 def del_fav(fid):
 
     # recibir info del request
     
-    fav = Favorites.query.get(fid)
+    fav = Todos.query.get(fid)
     if fav is None:
-        raise APIException('Favorite not found', status_code=404)
+        raise APIException('Todos not found', status_code=404)
 
     db.session.delete(fav)
 
--- src/models.py
+++ src/models.py
@@ -19,7 +19,7 @@ class User(db.Model):
         }
 
 
-class Favorites(db.Model):
+class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
 
